7ab0f472a6ebb437ef50e653aef19c65ceb5472b
Andrés1815 <ajaramillob@unicartagena.edu.co>
2026-01-16 16:18:15 -0500

    nuevo proyecto

---
+++ app.js
@@ -0,0 +1,26 @@
+let persona = [
+    {
+     Nombre: "Andres",
+     edad: 19,
+    }, 
+    {
+     Nombre: "Lucas",
+     edad: 18,
+    }, 
+    {
+     Nombre: "Ana",
+     edad: 17,
+    }, 
+    {
+     Nombre: "Daniela",
+     edad: 17,
+    }
+]
+
+let menores = persona.filter(p => p.edad <= 18);
+console.log(menores)
+
+
+
+let andres = 2
+console.log(andres)
